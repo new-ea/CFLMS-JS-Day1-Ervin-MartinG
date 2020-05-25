7bb37cef4e6035c521a3943729b23fa160f59bac
MartinG-hub <65162359+MartinG-hub@users.noreply.github.com>
2020-05-25 12:03:44 +0200

    BasicEx1-4, AdvancedEx1+2 finished

---
+++ Advanced/Aex.js
@@ -0,0 +1,65 @@
+var autos = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+
+document.write(autos.sort());
+
+
+
+var fruits = ["apple", "banana", "kiwi"];
+
+fruits.push("orange");
+
+document.write(fruits + " <br>");
+
+fruits.pop();
+
+
+document.write(fruits + " <br>");
+
+
+
+var animals = ["monkey", "horse", "dog"];
+
+document.write(animals.sort());
+
+document.write(animals + " <br>");
+
+
+animals.unshift("cat");
+
+document.write(animals + " <br>");
+
+
+var fruits1 = "mango/cherries/kiwi/grapes/pear/orange/lemon";
+
+document.write(fruits1 + " <br>");
+
+console.log(fruits1);
+
+//fruits1.split(" ");
+
+console.log(fruits1);
+
+//fruits1.split("/");
+
+console.log(fruits1);
+
+var temp = new Array();
+
+temp = fruits1.split("/");
+
+//console.log(fruits1);
+
+console.log(temp);
+
+document.write(temp[1]);
+
+
+
+//console.log(fruits1.split("\"));
+
+//var temp = new Array();
+
+//temp = fruits1.split("\");
+
+//console.log(temp);
+
---
+++ Basic2.js
@@ -0,0 +1,11 @@
+
+
+// Basic Exercise 4 begins
+
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+var value = "The most valuable player of the match is ";
+
+document.write(value + players[2]);
+
+
