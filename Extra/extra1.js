86fcb7feba69686259c62aa6aba7e4326fb4eb20
MartinG-hub <65162359+MartinG-hub@users.noreply.github.com>
2020-05-25 13:47:35 +0200

    Extra Exercises 1+2

---
+++ Extra/extra1.js
@@ -0,0 +1,14 @@
+var sentence = "Hey there, i am a javascript developer, and i live in vienna"
+
+console.log(sentence.substring(11,38));
+
+document.write(sentence.substring(11, 38));
+
+document.write(" \n")
+
+
+var fruits = ["apple","banana", "kiwi"];
+
+fruits.splice(2,0, "orange", "strawberry");
+
+document.write(fruits);
